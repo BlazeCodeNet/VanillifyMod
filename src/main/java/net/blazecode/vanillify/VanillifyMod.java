9cc46c8c929d6b7f84517a31fc03ab097cd3546f
ElementalVenom <elementalvenom1@gmail.com>
2021-08-27 02:38:49 -0500

    Reverted 1.3.48 Changes - 1.3.50 Reverted 1.3.48 changes that added BlockActionListener due to the fact that java doesnt use interfaces in the same way that im used to with c#.

--- src/main/java/net/blazecode/vanillify/VanillifyMod.java
+++ src/main/java/net/blazecode/vanillify/VanillifyMod.java
@@ -1,13 +1,8 @@
 package net.blazecode.vanillify;
 
-import net.blazecode.vanillify.api.listeners.BlockActionListener;
 import net.blazecode.vanillify.commands.VanillaCommand;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.minecraft.block.Block;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.ActionResult;
 
 import java.util.logging.Logger;
 
@@ -22,22 +17,6 @@ public class VanillifyMod implements DedicatedServerModInitializer
             {
                 VanillaCommand.register(dispatcher);
             }
-        });
-
-        AttackBlockCallback.EVENT.register( (player, world, hand, pos, direction) ->
-        {
-            if(!world.isClient)
-            {
-                ServerPlayerEntity srvPlr = (ServerPlayerEntity )player;
-                Block blk = world.getBlockState( pos ).getBlock();
-                if( blk instanceof BlockActionListener )
-                {
-                    BlockActionListener l = (BlockActionListener ) blk;
-
-                    l.onPunch(srvPlr, pos);
-                }
-            }
-            return ActionResult.PASS;
         });
     }
     
